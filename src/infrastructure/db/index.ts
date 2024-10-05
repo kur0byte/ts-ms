a473c4aa38e431179757c679f0b27d786d3c5727
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-05 11:50:32 -0500

    feat: add typeORM config

---
+++ src/infrastructure/db/index.ts
@@ -0,0 +1,24 @@
+import {Customer} from '@Customer/domain/Customer';
+import {DataSource} from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'myuser',
+  password: 'mypassword',
+  database: 'myapp',
+  synchronize: true,
+  logging: false,
+  entities: [Customer],
+  subscribers: [],
+  migrations: [],
+});
+
+export const initDatabase = async () => {
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Database is connected');
+    })
+    .catch(error => console.log(error));
+};
